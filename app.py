0f71d1efaea1da22c04cb0c554793973eacd70b7
rajababu843 <131129836+rajababu843@users.noreply.github.com>
2024-02-29 21:42:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,14 @@ st.title("Sentiment Analysis")
 def get_model():
     MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
     tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    labels=[]
+    mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+    with urllib.request.urlopen(mapping_link) as f:
+    html = f.read().decode('utf-8').split("\n")
+    csvreader = csv.reader(html, delimiter='\t')
+    labels = [row[1] for row in csvreader if len(row) > 1]
     model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    
     return tokenizer,model
 
 
